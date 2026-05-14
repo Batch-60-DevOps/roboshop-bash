301402d3a693a8e063d3da90da9e1356c9c82bff
sivakrishna0 <sivachowz@icloud.com>
2026-05-14 18:31:22 +0530

    mongod

--- mongodb.sh
+++ mongodb.sh
@@ -2,6 +2,7 @@
 echo "configuration managment of the mongodb application"
 ID=$(id -u)
 COMPONENT="mongodb"
+LOG="/tmp/$COMPONENT.log"
 if [ $ID -ne 0 ]; then
 echo -e "\e[31m you should run this script as root or with sudo privileage \e[0m]"
 echo -e "Example: Usage: \e[36msudo bash $0 \e[0m"
@@ -18,20 +19,20 @@ stat() {
 }
 
 echo "installing mongodb repo"
-cp mongo.repo /etc/yum.repos.d/mongodb.repo &>> /tmp/$COMPONENT.log
+cp mongo.repo /etc/yum.repos.d/mongodb.repo &>> $LOG
 stat $?
 
 echo "installing mongodb"
-dnf install mongodb-org -y &>> /tmp/$COMPONENT.log
+dnf install mongodb-org -y &>> $LOG
 stat $?
 
 echo "updating the mongodb config file"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> /tmp/$COMPONENT.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOG
 stat $?
 
 echo "starting mongodb service"
-systemctl enable mongod &>> /tmp/$COMPONENT.log
-systemctl start mongod &>> /tmp/$COMPONENT.log
+systemctl enable mongod &>> $LOG
+systemctl start mongod &>> $LOG
 stat $?
 
 echo -e "\e[32m mongodb setup is completed \e[0m"
