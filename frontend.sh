ac95fa993560fa96e11aaaf26e0aeebfe236db07
sivakrishna0 <sivachowz@icloud.com>
2026-05-14 17:18:37 +0530

    mongod

--- frontend.sh
+++ frontend.sh
@@ -2,6 +2,8 @@
 echo "configuration managment of the frontend application"
 ID=$(id -u)
 COMPONENT="frontend"
+LOG="/tmp/${COMPONENT}.log"
+
 if [ $ID -ne 0 ]; then
 echo -e "\e[31m you should run this script as root or with sudo privileage \e[0m]"
 echo -e "Example: Usage: \e[36msudo bash $0 \e[0m"
@@ -18,31 +20,35 @@ stat() {
 }
 
 echo "disabling the default nginx repo"
-dnf module disable nginx -y &>> /tmp/$COMPONENT.log
+dnf module disable nginx -y &>> $LOG
 stat $?
 
 echo "enable the nginx 24 version"
-dnf module enable nginx:1.24 -y &>> /tmp/$COMPONENT.log
+dnf module enable nginx:1.24 -y &>> $LOG
 stat $?
 
 echo "installing nginx"
-dnf install nginx -y &>> /tmp/$COMPONENT.log  
+dnf install nginx -y &>> $LOG
 stat $?
 
 echo "downloading the frontend content"
-curl -L -o /tmp/frontend.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip &>> /tmp/$COMPONENT.log
+curl -L -o /tmp/frontend.zip https://stan-robotshop.s3.amazonaws.com/$COMPONENT-v3.zip &>> $LOG
 stat $?
 
 echo "removing the old content"
-rm -rf /usr/share/nginx/html/* &>> /tmp/$COMPONENT.log
+rm -rf /usr/share/nginx/html/* &>> $LOG
 
 echo "extracting the downloaded content"
-unzip /tmp/frontend.zip -d /usr/share/nginx/html/ &>> /tmp/$COMPONENT.log
+unzip /tmp/frontend.zip -d /usr/share/nginx/html/ &>> $LOG
+stat $?
+
+echo -n "Configuring $COMPONENT proxy file"
+cp nginx.conf /etc/nginx/nginx.conf
 stat $?
 
 echo "starting nginx service"
-systemctl enable nginx &>> /tmp/$COMPONENT.log
-systemctl start nginx &>> /tmp/$COMPONENT.log
+systemctl enable nginx &>> $LOG
+systemctl start nginx &>> $LOG
 stat $?
 
 echo -e "\e[32m frontend setup is completed \e[0m"
